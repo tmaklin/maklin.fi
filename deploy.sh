bc8cc408860f42b2ac40811aefa86bad7aec3a7b
Tommi Mäklin <tommi@maklin.fi>
2023-10-17 23:19:02 +0300

    Add converting academic cv to pdf.

--- deploy.sh
+++ deploy.sh
@@ -31,6 +31,26 @@ docker run --user $(id -u $(whoami)):$(id -g $(whoami)) \
 ## Update the download link in content/resume.md
 sed -i "s/resume_placeholder.pdf/$resume_filename.pdf/g" content/resume.md
 
+## Convert the academic CV to a pdf
+md_in=tmp-in.md
+echo "# Tommi M&auml;klin" > $md_in
+sed '1,7d' content/academic-cv.md | sed 's/[&]ZeroWidthSpace[;]//g' >> $md_in
+sed -i 's/^[>]//g' $md_in
+sed -i "s/<script src=\"\/js\/contact_me.js\"><\/script><noscript>[[]Turn on JavaScript to see my email address.[]]<\/noscript>/$1/g" $md_in
+sed -i "s/<script src=\"\/js\/call_me.js\"><\/script><noscript>[[]Turn on JavaScript to see my phone number.[]]<\/noscript>/$2/g" $md_in
+##sed -i "s/<br><br>//g" $md_in
+sed -i "s/$2.*$/$2/g" $md_in
+sed -i "s/Heldata/<br>Heldata/g" $md_in
+
+docker run --user $(id -u $(whoami)):$(id -g $(whoami)) \
+       -it -v $(pwd):/resume there4/markdown-resume \
+       md2resume pdf --template unstyled -o academic_cv_tommi_maklin_$(date +%Y_%m_%d) $md_in static/documents/ > /dev/null
+
+cv_filename=academic_cv_tommi_maklin_$(date +%Y_%m_%d)
+
+## Update the download link in content/academic-cv.md
+sed -i "s/academic_cv_placeholder.pdf/$cv_filename.pdf/g" content/academic-cv.md
+
 hugo
 
 mv public $sitename
